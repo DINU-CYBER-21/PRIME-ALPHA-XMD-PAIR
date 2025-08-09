60a70a16d5b2c5cfd03ca0b4d05c6605e62e570a
DINU-CYBER-21 <dinuwa280@gmail.com>
2025-08-09 19:05:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 4000;
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+
 let code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
+
+// Comment out or define 'server' if needed
+// const server = require('./server');
+// app.use('/server', server);
+
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/pair', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'pair.html'));
+});
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'main.html'));
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
+  console.log(`Don't Forget To Give Star
+Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
